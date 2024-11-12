dca2a4f4aeb5edfa338485599ac2608614337f02
Michael <126069497+Michalkcb@users.noreply.github.com>
2024-11-12 20:26:07 +0100

    training gnl 12.11

--- get_next_line.c
+++ get_next_line.c
@@ -1,78 +1,59 @@
-/* ************************************************************************** */
-/*                                                                            */
-/*                                                        :::      ::::::::   */
-/*   get_next_line.c                                    :+:      :+:    :+:   */
-/*                                                    +:+ +:+         +:+     */
-/*   By: mbany <mbany@student.42warsaw.pl>          +#+  +:+       +#+        */
-/*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2024/03/19 13:00:52 by mbany             #+#    #+#             */
-/*   Updated: 2024/11/10 14:35:44 by mbany            ###   ########.fr       */
-/*                                                                            */
-/* ************************************************************************** */
-
-///=∂/\/\[](_)§ /\/\@|V †|-|@¯|¯ /-/!570®1<|-\£1_`/ ¢@/\/\ε vv!7}{ ???
-
 #include "get_next_line.h"
 
 int slen(char *s)
 {
 	int i = 0;
-	while(s[i])
+	while (s[i])
 		i++;
 	return (i);
 }
-
-char	*ft_strdup(char *src)
+char *sdup(char *src)
 {
-	char	*dest;
-	dest = (char *)malloc(sizeof(char) * (slen(src) + 1));
 	int i = 0;
+	char *dest = (char *)malloc(sizeof(char) * (slen(src) + 1));
 	while (src[i])
 	{
-	   dest[i] = src[i];
-	   i++;
+		dest[i] = src[i];
+		i++;
 	}
 	dest[i] = '\0';
-	return (dest);
+	return (dest);	
 }
-
-char	*get_next_line(int fd)
+char *gnl(int fd)
 {
-	static char	buffer[BUFFER_SIZE];
-	char		line[70000];
-	static int	buffer_read;
-	static int 	buffer_pos;
-	int			i;
+	static char buffer[BUFFER_SIZE];
+	char line[70000];
+	static int buffer_read;
+	static int buffer_pos;
+	int i = 0;
 
-	i = 0;
-	if (fd < 0 || BUFFER_SIZE <= 0)
-		return (NULL);
+	if (fd < 0 || BUFFER_SIZE <=0)
+		return NULL;
 	while (1)
 	{
 		if (buffer_pos >= buffer_read)
 		{
-			buffer_read = read(fd, buffer, BUFFER_SIZE);
+			buffer_read = read(fd,buffer, BUFFER_SIZE);
 			buffer_pos = 0;
 			if (buffer_read <= 0)
-				break ;
+				break;
 		}
 		if (line[i] == '\n')
-			break ;
+			break;
 		line[i] = buffer[buffer_pos++];
 		i++;
 	}
 	line[i] = '\0';
 	if (i == 0)
-		return (NULL);
-	return (ft_strdup(line));
+		return NULL;
+	return (sdup(line));
 }
-
 int main()
 {
-    int fd = open("./test.txt", O_RDONLY);
-	char *line = get_next_line(fd);
-    printf("%s\n", line);
+	int fd = open("./text.html", O_RDONLY);
+	char *line = gnl(fd);
+	printf("%s\n", line);
 	free(line);
 	close(fd);
-    return (0);
+	return(0);
 }
